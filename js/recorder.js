40089a9de5a35e691c92af2469266e1f30aea805
yodalee <lc85301@gmail.com>
2015-04-09 20:45:43 +0800

    add recorder.js
    basic wrap of audio buffer element

---
+++ js/recorder.js
@@ -0,0 +1,12 @@
+var Recorder = function(source) {
+  var bufferSize = 4096;
+  var numOfInputChannels = 2;
+  var numOfOutputChannels = 2;
+  this.context = source.context;
+
+  this.node = (this.context.createScriptProcessor ||
+      this.context.createJavaScriptNode).call(
+      this.context, bufferSize, numOfInputChannels, numOfOutputChannels);
+
+  var recording = false;
+}
