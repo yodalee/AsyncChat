1dd6a755e787d68a29922ec33f68d85491205f98
yodalee <lc85301@gmail.com>
2015-12-15 16:27:34 +0800

    add login requirement to index page

--- recorder.py
+++ recorder.py
@@ -4,6 +4,8 @@ import os
 import webapp2
 import jinja2
 
+from google.appengine.ext.webapp import util
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -11,6 +13,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 class MainPage(webapp2.RequestHandler):
+    @util.login_required
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('template/AsyncChat.html')
         self.response.write(template.render())
